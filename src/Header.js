227e40ed5ffb64969ade7fc81e5953fb79822428
themindasrimal <themindasrimal@gmail.com>
2021-07-17 15:41:31 +0530

    header-avatar

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import HeaderOption from './HeaderOption';
 import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import SmsIcon from '@material-ui/icons/Sms';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
         <div className='header'>
-            
+
             <div className="header__left">
                 <img src="https://cdn.freelogovectors.net/wp-content/uploads/2020/01/linkedin-logo.png" alt="" />
 
@@ -16,10 +22,16 @@ function Header() {
             </div>
 
             <div className="header__right">
-
+                <HeaderOption Icon={HomeIcon} title='Home'/>
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+                <HeaderOption Icon={SmsIcon} title='Messaging'/>
+                <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+                <HeaderOption avatar='https://wallpaperaccess.com/full/2213424.jpg' title='me'/>
+ 
             </div>
         </div>
     )
 }
 
-export default Header
+export default Header;
