0d90d49d123bc5c8c1eefee4176cd83fdbf1d23d
themindasrimal <themindasrimal@gmail.com>
2021-07-21 23:59:17 +0530

    feed-avatar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import Feed from './Feed';
+import Feed from './Feed'; 
 
 function App() {
   return (
--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Feed.css';
 import CreateIcon from '@material-ui/icons/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
+import Post from './Post';
 
 function Feed() {
     return (
@@ -13,8 +19,16 @@ function Feed() {
                         <button type='submit'>Send</button>
                     </form>
                 </div>
+                <div className="feed__inputOptions">
+                    <InputOption Icon={ImageIcon} title="Photo" color="#7085F9" /> 
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" /> 
+                    <InputOption Icon={EventNoteIcon} title="Video" color="#C0CBCD" /> 
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E" /> 
+                </div>
             </div>
-        </div>
+
+            <Post name='TS' description='test' message='this works'/>
+        </div> 
     )
 }
 
